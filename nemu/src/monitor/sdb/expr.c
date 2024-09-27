f717a69f2de4830b4cdabfd361d98a563d3eca2a
tracer-ysyx <tracer@ysyx.org>
2024-09-27 17:22:25 +0800

    > compile NEMU ysyx_22040000 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:22:25 up  4:10,  1 user,  load average: 0.35, 0.26, 0.18

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -105,6 +105,7 @@ static bool make_token(char *e) {
         default:
           strncpy(tokens[nr_token].str, substr_start, substr_len);
           tokens[nr_token].type = rules[i].token_type;
+          printf("%s", tokens[nr_token].str);
         }
 
         break;
