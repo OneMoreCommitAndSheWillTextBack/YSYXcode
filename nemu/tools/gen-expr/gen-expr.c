4bd2e41fbc50e4a1d463514fb699ba189b029bc1
tracer-ysyx <tracer@ysyx.org>
2024-09-28 14:22:43 +0800

    > compile NEMU ysyx_22040000 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:22:43 up  3:38,  1 user,  load average: 0.41, 0.23, 0.13

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -1,38 +1,85 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ *PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ *KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ *NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
+#include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <assert.h>
 #include <string.h>
+#include <time.h>
 
 // this should be enough
 static char buf[65536] = {};
 static char code_buf[65536 + 128] = {}; // a little larger than `buf`
-static char *code_format =
-"#include <stdio.h>\n"
-"int main() { "
-"  unsigned result = %s; "
-"  printf(\"%%u\", result); "
-"  return 0; "
-"}";
+static char *code_format = "#include <stdio.h>\n"
+                           "int main() { "
+                           "  unsigned result = %s; "
+                           "  printf(\"%%u\", result); "
+                           "  return 0; "
+                           "}";
+char buffer[10] = {};
+int pos = 0;
+void gen_num() {
+  int counts = rand() % 1000;
+  sprintf(buffer + 2, "%d  ", counts);
+  strcpy(buf + pos, buffer);
+  pos = pos + strlen(buffer);
+}
 
-static void gen_rand_expr() {
-  buf[0] = '\0';
+void gen_op() {
+  int choose = rand() % 4;
+  switch (choose) {
+  case 0:
+    buf[pos] = '+';
+    break;
+  case 1:
+    buf[pos] = '-';
+    break;
+  case 2:
+    buf[pos] = '*';
+    break;
+  case 3:
+    buf[pos] = '/';
+    break;
+  }
+  pos++;
+}
+
+void gen(char c) {
+  buf[pos] = c;
+  pos++;
+}
+
+static void gen_rand_expr(int time) {
+  switch (rand() % 2) {
+  case 0:
+    gen_num();
+    break;
+  case 1:
+    gen('(');
+    gen_rand_expr(time - 1);
+    gen(')');
+    break;
+  case 2:
+    gen_rand_expr(time - 2);
+    gen_op();
+    gen_rand_expr(time - 2);
+    break;
+  default:
+    assert(0);
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -43,8 +90,8 @@ int main(int argc, char *argv[]) {
     sscanf(argv[1], "%d", &loop);
   }
   int i;
-  for (i = 0; i < loop; i ++) {
-    gen_rand_expr();
+  for (i = 0; i < loop; i++) {
+    gen_rand_expr(10);
 
     sprintf(code_buf, code_format, buf);
 
@@ -54,7 +101,8 @@ int main(int argc, char *argv[]) {
     fclose(fp);
 
     int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
-    if (ret != 0) continue;
+    if (ret != 0)
+      continue;
 
     fp = popen("/tmp/.expr", "r");
     assert(fp != NULL);
