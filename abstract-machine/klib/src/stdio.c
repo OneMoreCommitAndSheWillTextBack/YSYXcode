5d6832f71cd2508fba0e7b536e69af3849cb4e4f
tracer-ysyx <tracer@ysyx.org>
2024-10-17 14:03:24 +0800

    > compile NEMU ysyx_22040000 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:03:24 up 20 min,  1 user,  load average: 0.67, 0.71, 0.77

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,28 +1,79 @@
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdarg.h>
+#include <stdio.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  char buffer[512];
+  va_list ap;
+  va_start(ap, fmt);
+  int size = vsprintf(buffer, fmt, ap);
+  va_end(ap);
+  return size;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  return vsnprintf(out, 512, fmt, ap);
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int size = vsnprintf(out, 512, fmt, ap);
+  va_end(ap);
+  return size;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int size = vsnprintf(out, n, fmt, ap);
+  va_end(ap);
+  return size;
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  size_t outn = 0;
+  size_t fmtn = 0;
+  int intarg = 0;
+  char *chararg = NULL;
+  int i = 0;
+  char buf[32];
+  while (outn < n) {
+    switch (fmt[fmtn]) {
+    case '%':
+      fmtn++;
+      switch (fmt[fmtn]) {
+      case 'd':
+        intarg = va_arg(ap, int);
+        if (intarg < 0) {
+          intarg = -intarg;
+          out[outn++] = '-';
+        }
+        for (i = 0; intarg > 0; intarg = intarg / 10)
+          buf[i++] = (intarg % 10) - '0';
+        for (; i >= 0; i--)
+          out[outn++] = buf[i];
+        break;
+      case 's':
+        chararg = va_arg(ap, char *);
+        i = 0;
+        while (chararg[i] != '\0')
+          out[outn++] = chararg[i++];
+        break;
+      default:
+        return -1;
+      }
+      break;
+    default:
+      out[outn++] = fmt[fmtn++];
+    }
+  }
+  out[outn] = '\0';
+  return outn;
 }
 
 #endif
