cb141b2e125b4295dc47dccf9e5c929714db8fb9
tracer-ysyx <tracer@ysyx.org>
2024-11-12 11:52:48 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:52:48 up  1:12,  1 user,  load average: 1.50, 0.87, 0.56

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -18,7 +18,7 @@
 #include <isa.h>
 #include <cpu/cpu.h>
 #include <memory/paddr.h>
-#include <utils.h>
+// #include <utils.h>
 #include <difftest-def.h>
 
 void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
