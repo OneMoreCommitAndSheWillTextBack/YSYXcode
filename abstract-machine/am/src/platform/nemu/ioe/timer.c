4f1629dc0d1db9a5f58e5bc8e54a365d15a2d6e7
tracer-ysyx <tracer@ysyx.org>
2024-11-12 13:28:08 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:28:08 up  2:47,  1 user,  load average: 0.49, 0.50, 0.59

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,18 +1,23 @@
+#include "amdev.h"
+#include "klib-macros.h"
 #include <am.h>
 #include <nemu.h>
+#include <stdint.h>
 
-void __am_timer_init() {
-}
+void __am_timer_init() {}
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  uintptr_t rtcbase = (uintptr_t)&_pmem_start + RTC_ADDR;
+  uint64_t uptimehgh = *(uint32_t *)(rtcbase + 1);
+  uint64_t uptimelow = *(uint32_t *)(rtcbase);
+  uptime->us = (uptimehgh << 32) + uptimelow;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
   rtc->second = 0;
   rtc->minute = 0;
-  rtc->hour   = 0;
-  rtc->day    = 0;
-  rtc->month  = 0;
-  rtc->year   = 1900;
+  rtc->hour = 0;
+  rtc->day = 0;
+  rtc->month = 0;
+  rtc->year = 1900;
 }
