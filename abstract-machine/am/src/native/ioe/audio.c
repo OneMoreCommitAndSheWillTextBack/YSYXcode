328681baa4578c7083045be2c158131d5411cfd6
tracer-ysyx <tracer@ysyx.org>
2024-11-22 18:26:19 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:26:19 up 18 min,  1 user,  load average: 1.56, 2.01, 1.67

--- abstract-machine/am/src/native/ioe/audio.c
+++ abstract-machine/am/src/native/ioe/audio.c
@@ -1,8 +1,9 @@
+// clang-format off
 #define _GNU_SOURCE
-#include <SDL2/SDL.h>
 #include <fcntl.h>
-#include <klib.h>
 #include <unistd.h>
+#include <klib.h>
+#include <SDL2/SDL.h>
 
 static int rfd = -1, wfd = -1;
 static volatile int count = 0;
@@ -17,13 +18,11 @@ void __am_audio_init() {
 
 static void audio_play(void *userdata, uint8_t *stream, int len) {
   int nread = len;
-  if (count < len)
-    nread = count;
+  if (count < len) nread = count;
   int b = 0;
   while (b < nread) {
     int n = read(rfd, stream, nread);
-    if (n > 0)
-      b += n;
+    if (n > 0) b += n;
   }
 
   count -= nread;
@@ -36,8 +35,7 @@ static void audio_write(uint8_t *buf, int len) {
   int nwrite = 0;
   while (nwrite < len) {
     int n = write(wfd, buf, len);
-    if (n == -1)
-      n = 0;
+    if (n == -1) n = 0;
     count += n;
     nwrite += n;
   }
@@ -60,7 +58,9 @@ void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
   }
 }
 
-void __am_audio_status(AM_AUDIO_STATUS_T *stat) { stat->count = count; }
+void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
+  stat->count = count;
+}
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
   int len = ctl->buf.end - ctl->buf.start;
