7340cb396a38bb2c91e07148adab36e94b6a0ab3
tracer-ysyx <tracer@ysyx.org>
2024-11-29 19:09:04 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:09:04 up  4:33,  1 user,  load average: 0.24, 0.10, 0.02

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -7,6 +7,7 @@ static Context *(*user_handler)(Event, Context *) = NULL;
 Context *__am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
+    printf("into am irq handle, mcause is %d\n", c->mcause);
     switch (c->mcause) {
       // clang-format off
       case -1: ev.event = EVENT_YIELD; printf("yield!\n"); break;
