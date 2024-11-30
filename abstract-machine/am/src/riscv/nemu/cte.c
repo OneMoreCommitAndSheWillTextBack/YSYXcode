13076db1b94db0955179c023d896245af30a501d
tracer-ysyx <tracer@ysyx.org>
2024-11-30 11:29:30 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:29:30 up 34 min,  1 user,  load average: 0.16, 0.08, 0.06

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -1,6 +1,7 @@
 #include <am.h>
 #include <klib.h>
 #include <riscv/riscv.h>
+#include <stdint.h>
 
 static Context *(*user_handler)(Event, Context *) = NULL;
 
@@ -35,7 +36,19 @@ bool cte_init(Context *(*handler)(Event, Context *)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  return NULL;
+  uintptr_t *tmp = (uintptr_t *)kstack.end;
+  for (int i = 0; i < 33; i++) {
+    *tmp = 0;
+    tmp++;
+  }
+  *tmp = 0x1800; // mstatus = 0x1800
+  tmp++;
+  *tmp = (uintptr_t)entry; // mepc = entry
+  tmp++;
+  *tmp = 0; // pdir
+  tmp++;
+
+  return (Context *)tmp;
 }
 
 void yield() {
