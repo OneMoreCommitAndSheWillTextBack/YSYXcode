53795fee46fefb5ea41c1b0e0441c878d4e55682
tracer-ysyx <tracer@ysyx.org>
2024-12-05 18:30:22 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:30:22 up 25 min,  1 user,  load average: 0.21, 0.39, 0.46

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -20,6 +20,10 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  if (screen_h_nemu == 0 && screen_w_nemu == 0) {
+    screen_w_nemu = 320;
+    screen_h_nemu = 200;
+  }
   uint32_t *pix = (uint32_t *)ctl->pixels;
   uint32_t *fdb = (uint32_t *)FB_ADDR;
   size_t fb_h = ctl->h;
