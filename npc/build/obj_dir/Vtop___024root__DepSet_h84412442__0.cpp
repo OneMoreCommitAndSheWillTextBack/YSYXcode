eebc4557cc46038b5b8950b695b16e912505ab47
tracer-ysyx <tracer@ysyx.org>
2024-12-29 23:38:45 +0800

    > sim RTL ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:38:45 up  2:43,  1 user,  load average: 0.66, 0.51, 0.49

--- npc/build/obj_dir/Vtop___024root__DepSet_h84412442__0.cpp
+++ npc/build/obj_dir/Vtop___024root__DepSet_h84412442__0.cpp
@@ -131,15 +131,16 @@ VL_INLINE_OPT void Vtop___024root___nba_sequent__TOP__0(Vtop___024root* vlSelf) 
     }
     if (vlSelf->rst) {
         vlSelf->__Vdly__top__DOT__ifu0__DOT__pcbridge = 0x80000000U;
-    } else if (vlSelf->top__DOT__ifu0__DOT__infetch0__DOT__state) {
-        vlSelf->top__DOT__ifu0__DOT__pc_valid = 0U;
-    } else {
+    } else if (((~ (IData)(vlSelf->top__DOT__ifu0__DOT__infetch0__DOT__state)) 
+                & (0U != vlSelf->top__DOT__ifu0__DOT__instbridge))) {
         if ((vlSelf->top__DOT__npc == vlSelf->top__DOT__ifu0__DOT__pcbridge)) {
             Vtop___024unit____Vdpiimwrap_ret_TOP____024unit(0U);
         }
         vlSelf->top__DOT__ifu0__DOT__pc_valid = 1U;
         vlSelf->__Vdly__top__DOT__ifu0__DOT__pcbridge 
             = vlSelf->top__DOT__npc;
+    } else {
+        vlSelf->top__DOT__ifu0__DOT__pc_valid = 0U;
     }
     if ((1U & (~ (IData)(vlSelf->rst)))) {
         if (vlSelf->top__DOT__regfile__DOT____Vcellinp__registers0__ew) {
