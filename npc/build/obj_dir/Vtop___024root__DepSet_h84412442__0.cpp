e2a55dd896418c4d26a4d7647ffb635afa4853fa
tracer-ysyx <tracer@ysyx.org>
2025-01-01 20:32:02 +0800

    > sim RTL ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:32:02 up  1:06,  1 user,  load average: 1.04, 0.78, 0.65

--- npc/build/obj_dir/Vtop___024root__DepSet_h84412442__0.cpp
+++ npc/build/obj_dir/Vtop___024root__DepSet_h84412442__0.cpp
@@ -420,8 +420,9 @@ VL_INLINE_OPT void Vtop___024root___nba_sequent__TOP__1(Vtop___024root* vlSelf) 
     }
     if (vlSelf->rst) {
         __Vdly__top__DOT__ifu0__DOT__pcbridge = 0x80000000U;
-    } else if ((1U & ((~ (IData)(vlSelf->top__DOT__ifu0__DOT__infetch0__DOT__state)) 
-                      & (~ (IData)(vlSelf->top__DOT__wbu0__DOT__mem0__DOT__state))))) {
+    } else if (((~ (IData)(vlSelf->top__DOT__ifu0__DOT__infetch0__DOT__state)) 
+                & ((~ (IData)(vlSelf->top__DOT__wbu0__DOT__mem0__DOT__state)) 
+                   & (0U != vlSelf->top__DOT__ifu0__DOT__instbridge)))) {
         if ((vlSelf->top__DOT__npc == vlSelf->top__DOT__ifu0__DOT__pcbridge)) {
             Vtop___024unit____Vdpiimwrap_ret_TOP____024unit(0U);
         }
--- npc/vsrc/ifu/infetch.v
+++ npc/vsrc/ifu/infetch.v
@@ -36,5 +36,5 @@ module infetch(
   end
 
   assign valid_to = inst != 0;
-  assign ready_to = ready_from & valid_to;
+  assign ready_to = state == WAIT_FOR_INST & ready_from & valid_to;
 endmodule
