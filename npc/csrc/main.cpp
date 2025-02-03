814a65c6d8a13b2afabf133716a42582282ff1fc
tracer-ysyx <tracer@ysyx.org>
2025-02-04 01:21:39 +0800

    > sim RTL ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:21:39 up 12:51,  1 user,  load average: 0.69, 0.52, 0.47

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,11 +1,5 @@
 #include "common.h"
 
-extern "C" void flash_read(int32_t addr, int32_t *data) { assert(0); }
-extern "C" void mrom_read(int32_t addr, int32_t *data) {
-  printf("call the function mrom_read\n");
-  *data = 0b00000000000100000000000001110011;
-}
-
 extern int times;
 extern Npc *npc;
 int main(int argc, char *argv[]) {
