a64306f98e1f9d076c1ea796a84c42d77afe23d9
tracer-ysyx <tracer@ysyx.org>
2025-02-05 16:34:44 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:34:44 up  2:18,  1 user,  load average: 0.27, 0.35, 0.40

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -35,8 +35,10 @@ typedef struct diff_context {
 
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
   if (direction == DIFFTEST_TO_REF) {
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++){
       paddr_write(addr + i, 2, *((uint8_t *)buf + i));
+      printf("difftest_memcpy: 0x%x <- %x\n", addr + i, paddr_read(addr + i, 4));
+    }
   } else {
     assert(0);
   }
