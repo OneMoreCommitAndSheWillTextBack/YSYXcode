34e7212d6367c80ba6b94c8f944facf51c03da58
tracer-ysyx <tracer@ysyx.org>
2025-02-05 23:43:52 +0800

    > sim RTL ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:43:52 up  9:27,  1 user,  load average: 0.47, 0.54, 0.44

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -14,6 +14,28 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 #endif
 static const char mainargs[] = MAINARGS;
 
+extern char _sdata_lma;
+extern char _bss_lma;
+extern char _sdata_vma;
+extern char _edata_vma;
+extern char _sbss_vma;
+extern char _ebss_vma;
+
+void loader_init() {
+  //复制.data段（LMA->VMA）
+  char *src = (char*)&_sdata_lma;  // MROM中的源地址
+  char *dst = (char*)&_sdata_vma;  // SRAM中的目标地址
+  unsigned int len = (uintptr_t)&_edata_vma - (uintptr_t)dst;
+
+  while (len--) {
+    *dst++ = *src++;
+  }
+
+  for (char *p = (char*)&_sbss_vma; p < (char*)&_ebss_vma; p++) {
+    *p = 0;
+  }
+}
+
 void putch(char ch) { *(char *)0x10000000 = ch; }
 
 void halt(int code) {
@@ -22,6 +44,7 @@ void halt(int code) {
 }
 
 void _trm_init() {
+  loader_init();
   int ret = main(mainargs);
   halt(ret);
 }
