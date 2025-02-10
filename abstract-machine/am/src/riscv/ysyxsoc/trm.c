fa25cd930033ae51baea778e647c3ce5265adaaf
tracer-ysyx <tracer@ysyx.org>
2025-02-10 22:09:21 +0800

    > sim RTL ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:09:21 up  3:09,  1 user,  load average: 0.56, 0.49, 0.42

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -80,10 +80,6 @@ void halt(int code) {
 
 void _trm_init() {
   *(unsigned char*)(UART_BASE + UART_LCR) = 0b10000011;
-  volatile char test = *(unsigned char*)(UART_BASE + UART_LCR);
-  if(test == 0b10000011){
-    halt(1);
-  };
   halt(0);
   loader_init();
   serial_init();
