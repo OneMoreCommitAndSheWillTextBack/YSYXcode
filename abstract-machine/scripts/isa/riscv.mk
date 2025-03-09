5184b2f22c767661120fa9566980a78397c2b1d2
tracer-ysyx <tracer@ysyx.org>
2025-03-09 17:16:16 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:16:16 up  1:02,  1 user,  load average: 1.52, 1.20, 1.29

--- abstract-machine/scripts/isa/riscv.mk
+++ abstract-machine/scripts/isa/riscv.mk
@@ -1,7 +1,7 @@
 CROSS_COMPILE := riscv64-linux-gnu-
 COMMON_CFLAGS := -fno-pic -march=rv32im_zicsr -mcmodel=medany -mstrict-align
 CFLAGS        += $(COMMON_CFLAGS) -static
-ASFLAGS       += $(COMMON_CFLAGS) -O2
+ASFLAGS       += $(COMMON_CFLAGS) -O1
 LDFLAGS       += -melf32lriscv
 
 # overwrite ARCH_H defined in $(AM_HOME)/Makefile
