e2c431c93454c2ea2d3a060fb81ae5dc3cfa87bd
tracer-ysyx <tracer@ysyx.org>
2026-01-20 16:16:10 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:16:10 up  6:11,  1 user,  load average: 0.18, 0.10, 0.09

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,18 +1,45 @@
 #include <common.h>
 #include "syscall.h"
 #include "am.h"
+#include "klib-macros.h"
 
+static char* sys_table[20] = {
+  [SYS_exit] = "sys_exit",
+  [SYS_yield] = "sys_yield",
+};
+
+static bool strace_on = false;
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
+
+  if(strace_on && sys_table[a[0]] != NULL)
+    Log("[strace] syscall %s, args1 %d, args %d, args %d", sys_table[a[0]]);
 
   switch (a[0]) {
-    case SYSCALL_EXIT:
+    case SYS_exit:
       halt(c->GPR2);
 
-    case SYSCALL_YIELD: 
+    case SYS_yield: 
       c->GPRx = 0;
+      break;
+
+    case SYS_write:
+      if(a[1] == 1 || a[1] == 0) {
+        char *buf = (char *)a[2];
+        unsigned int len = a[3];
+        int write_counter = 0;
+        for(write_counter=0;write_counter<len;write_counter++){
+          putstr(&buf[write_counter]);
+        }
+        c->GPRx = write_counter;
+      } else {
+        panic("the write syscall not implement except stdin and stderr");
+      }
       break;
 
     default: panic("Unhandled syscall ID = %d", a[0]);
