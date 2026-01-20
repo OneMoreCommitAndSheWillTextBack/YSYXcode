756a2019d0c4fa779a0387265dbf26d954a39acb
tracer-ysyx <tracer@ysyx.org>
2026-01-20 15:14:07 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:14:07 up  5:09,  1 user,  load average: 0.18, 0.11, 0.17

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,10 +4,10 @@
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD:
-      printf("yield\n");
+      Log("yield\n");
       break;
     case EVENT_SYSCALL:
-      printf("meet a syscall\n");
+      Log("meet a syscall\n");
       do_syscall(c);
       break;
     default: panic("Unhandled event ID = %d", e.event);
