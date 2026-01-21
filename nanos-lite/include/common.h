adb56c389435c2c4d0da2de2f5ab89f96e151e30
tracer-ysyx <tracer@ysyx.org>
2026-01-21 21:24:36 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:24:36 up 12:07,  1 user,  load average: 0.35, 0.20, 0.12

--- nanos-lite/include/common.h
+++ nanos-lite/include/common.h
@@ -16,4 +16,7 @@
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 
+// syscall.c
+void do_syscall(Context *c);
+
 #endif
