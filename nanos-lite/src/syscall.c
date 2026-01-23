32d050d67a1c7c7b11da626863ddd28f8478f693
tracer-ysyx <tracer@ysyx.org>
2026-01-23 22:47:23 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:47:23 up 13:44,  1 user,  load average: 0.02, 0.07, 0.08

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -53,7 +53,9 @@ void do_syscall(Context *c) {
       break;
 
     case SYS_gettimeofday:
+      Log("get to here");
       c->GPRx = get_time((struct timeval *)a[1], (struct timezone *)a[2]);
+      Log("to here");
       break;
 
     default: panic("Unhandled syscall ID = %d", a[0]);
