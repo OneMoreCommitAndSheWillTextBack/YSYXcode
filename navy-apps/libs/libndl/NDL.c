744c2595de30d6ffff238aaf50cc8cff687af92a
tracer-ysyx <tracer@ysyx.org>
2026-01-24 14:59:17 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:59:17 up  6:13,  1 user,  load average: 0.09, 0.19, 0.15

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -21,7 +21,7 @@ uint32_t NDL_GetTicks() {
 
 int NDL_PollEvent(char *buf, int len) {
   FILE *fp = open("/dev/events", 0, 0);
-  return fread(buf, len, 1, fp);
+  return read(buf, len, 1, fp);
 }
 
 void NDL_OpenCanvas(int *w, int *h) {
