bf288cfe03f7c661f31b556b355d6a22eff8934d
tracer-ysyx <tracer@ysyx.org>
2026-02-21 16:33:30 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:33:30 up  6:59,  5 users,  load average: 0.37, 0.82, 0.61

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -3,22 +3,19 @@
 #include <common.h>
 
 #if defined(MULTIPROGRAM) && !defined(TIME_SHARING)
-# define MULTIPROGRAM_YIELD() yield()
+#define MULTIPROGRAM_YIELD() yield()
 #else
-# define MULTIPROGRAM_YIELD()
+#define MULTIPROGRAM_YIELD()
 #endif
 
-#define NAME(key) \
-  [AM_KEY_##key] = #key,
+#define NAME(key) [AM_KEY_##key] = #key,
 
-static const char *keyname[256] __attribute__((used)) = {
-  [AM_KEY_NONE] = "NONE",
-  AM_KEYS(NAME)
-};
+static const char *keyname[256]
+    __attribute__((used)) = {[AM_KEY_NONE] = "NONE", AM_KEYS(NAME)};
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
   int write_counter = 0;
-  for(write_counter=0;write_counter<len;write_counter++){
+  for (write_counter = 0; write_counter < len; write_counter++) {
     putch(((char *)buf)[write_counter]);
   }
   return write_counter;
@@ -27,10 +24,10 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 int get_time(struct timeval *tv, struct timezone *tz) {
   AM_TIMER_UPTIME_T time;
   ioe_read(AM_TIMER_UPTIME, &time);
- 
-  if(tv == NULL || tz == NULL)
+
+  if (tv == NULL || tz == NULL)
     return -1;
-    
+
   tv->tv_sec = time.us / 1000;
   tv->tv_usec = time.us;
 
@@ -44,12 +41,13 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   AM_INPUT_KEYBRD_T kbd;
   ioe_read(AM_INPUT_KEYBRD, &kbd);
 
-  if(kbd.keycode == AM_KEY_NONE) {
+  if (kbd.keycode == AM_KEY_NONE) {
     return 0;
   }
 
-  char* prefix = (kbd.keydown == true) ? "kd" : "ku";
-  size_t res = snprintf((char *)buf, len, "%s %s\n", prefix, keyname[kbd.keycode]);
+  char *prefix = (kbd.keydown == true) ? "kd" : "ku";
+  size_t res =
+      snprintf((char *)buf, len, "%s %s\n", prefix, keyname[kbd.keycode]);
   // Log("events_read buf: %s", buf);
   return res;
 }
@@ -58,7 +56,8 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   char buffer[32];
   AM_GPU_CONFIG_T cfg;
   ioe_read(AM_GPU_CONFIG, &cfg);
-  size_t size = snprintf(buffer, sizeof(buffer), "WIDTH : %d\nHEIGHT:%d", cfg.width, cfg.height);
+  size_t size = snprintf(buffer, sizeof(buffer), "WIDTH : %d\nHEIGHT:%d",
+                         cfg.width, cfg.height);
   Log("dispinfo buffer size %d", size);
   return snprintf(buf, len, "%s", buffer);
 }
@@ -72,7 +71,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   draw.w = len / sizeof(uint32_t);
   draw.y = offset / cfg.width / sizeof(uint32_t);
   draw.x = offset - draw.y * cfg.width;
-  // Log("fs_wirte x: %d, y: %d", draw.x, draw.y);
+  Log("fs_wirte x: %d, y: %d", draw.x, draw.y);
   draw.pixels = (uint32_t *)buf;
   draw.sync = 1;
 
