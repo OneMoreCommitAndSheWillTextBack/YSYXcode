47ba479514cd83543287d226029fe0784ca99492
tracer-ysyx <tracer@ysyx.org>
2026-02-22 16:55:23 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:55:23 up  7:40,  9 users,  load average: 0.33, 0.24, 0.19

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -1,28 +1,38 @@
-#define SDL_malloc  malloc
-#define SDL_free    free
+#define SDL_malloc malloc
+#define SDL_free free
 #define SDL_realloc realloc
 
 #define SDL_STBIMAGE_IMPLEMENTATION
 #include "SDL_stbimage.h"
 
-SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
+SDL_Surface *IMG_Load_RW(SDL_RWops *src, int freesrc) {
   assert(src->type == RW_TYPE_MEM);
   assert(freesrc == 0);
   return NULL;
 }
 
-SDL_Surface* IMG_Load(const char *filename) {
-  return NULL;
+SDL_Surface *IMG_Load(const char *filename) {
+  FILE *fd = fopen(filename, "r");
+
+  if (fd == NULL) {
+    assert(false && "IMG load fopen failed");
+  }
+
+  fseek(fd, 0, SEEK_END);
+  unsigned int size = ftell(fd);
+
+  uint32_t *buf = malloc(size);
+  fread(buf, size, 1, fd);
+
+  SDL_Surface *res = STBIMG_LoadFromMemory(buf, size);
+  free(buf);
+  fclose(fd);
+
+  return res;
 }
 
-int IMG_isPNG(SDL_RWops *src) {
-  return 0;
-}
+int IMG_isPNG(SDL_RWops *src) { return 0; }
 
-SDL_Surface* IMG_LoadJPG_RW(SDL_RWops *src) {
-  return IMG_Load_RW(src, 0);
-}
+SDL_Surface *IMG_LoadJPG_RW(SDL_RWops *src) { return IMG_Load_RW(src, 0); }
 
-char *IMG_GetError() {
-  return "Navy does not support IMG_GetError()";
-}
+char *IMG_GetError() { return "Navy does not support IMG_GetError()"; }
--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -77,6 +77,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
     lseek(fd_fb, sizeof(uint32_t) * ((frame_buffer_y + i) * max_width + frame_buffer_x), SEEK_SET);
     write(fd_fb, (pixels + i * canvas_width), sizeof(uint32_t) * canvas_width);
   }
+  close(fd_fb);
 }
 
 void NDL_OpenAudio(int freq, int channels, int samples) {
