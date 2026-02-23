6ce9f5002d20db1d8fe16174038ec69d8f776b1c
tracer-ysyx <tracer@ysyx.org>
2026-02-23 10:14:23 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:14:23 up 56 min,  5 users,  load average: 0.35, 0.29, 0.23

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,16 +1,16 @@
-#include <proc.h>
-#include <elf.h>
-#include <stdio.h>
 #include "common.h"
 #include "debug.h"
 #include "fs.h"
+#include <elf.h>
+#include <proc.h>
+#include <stdint.h>
 
 #ifdef __LP64__
-# define Elf_Ehdr Elf64_Ehdr
-# define Elf_Phdr Elf64_Phdr
+#define Elf_Ehdr Elf64_Ehdr
+#define Elf_Phdr Elf64_Phdr
 #else
-# define Elf_Ehdr Elf32_Ehdr
-# define Elf_Phdr Elf32_Phdr
+#define Elf_Ehdr Elf32_Ehdr
+#define Elf_Phdr Elf32_Phdr
 #endif
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
@@ -18,14 +18,14 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
   Elf32_Ehdr ehdr;
   size_t size = fs_read(fd, &ehdr, sizeof(Elf32_Ehdr));
-  if(size != sizeof(Elf32_Ehdr)) {
+  if (size != sizeof(Elf32_Ehdr)) {
     panic("loader read szie not equ to size of ehdr");
   }
 
   // Check ELF magic number: 0x7f 'E' 'L' 'F'
-  if(ehdr.e_ident[0] != 0x7f || ehdr.e_ident[1] != 'E' || 
-     ehdr.e_ident[2] != 'L' || ehdr.e_ident[3] != 'F' || 
-     ehdr.e_type != ET_EXEC) {
+  if (ehdr.e_ident[0] != 0x7f || ehdr.e_ident[1] != 'E' ||
+      ehdr.e_ident[2] != 'L' || ehdr.e_ident[3] != 'F' ||
+      ehdr.e_type != ET_EXEC) {
     panic("the file format isnot elf execuable file");
   }
 
@@ -34,32 +34,37 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   uint32_t e_phnum = ehdr.e_phnum;
   uint32_t e_entry = ehdr.e_entry;
 
-  Log("ELF header: e_phoff = 0x%x, e_phnum = %d, e_entry = 0x%x", e_phoff, e_phnum, e_entry);
+  Log("ELF header: e_phoff = 0x%x, e_phnum = %d, e_entry = 0x%x", e_phoff,
+      e_phnum, e_entry);
 
-  for(int i=0;i<e_phnum;i++) {
+  for (int i = 0; i < e_phnum; i++) {
     uint32_t offset = i * sizeof(Elf32_Phdr);
     size = fs_lseek(fd, e_phoff + offset, SEEK_SET);
     size = fs_read(fd, &phdr, sizeof(Elf32_Phdr));
-    if(size != sizeof(Elf32_Phdr)) {
+    if (size != sizeof(Elf32_Phdr)) {
       panic("loader read size not equ to size of phdr");
     }
 
-    // Log("phdr: p_type = 0x%x, p_offset = 0x%x, p_vaddr = 0x%x, p_paddr = 0x%x, p_filesz = 0x%x, p_memsz = 0x%x, p_flags = 0x%x",
-        // phdr.p_type, phdr.p_offset, phdr.p_vaddr, phdr.p_paddr, phdr.p_filesz, phdr.p_memsz, phdr.p_flags);
+    // Log("phdr: p_type = 0x%x, p_offset = 0x%x, p_vaddr = 0x%x, p_paddr =
+    // 0x%x, p_filesz = 0x%x, p_memsz = 0x%x, p_flags = 0x%x", phdr.p_type,
+    // phdr.p_offset, phdr.p_vaddr, phdr.p_paddr, phdr.p_filesz, phdr.p_memsz,
+    // phdr.p_flags);
 
-    if(phdr.p_type == PT_LOAD) {
+    if (phdr.p_type == PT_LOAD) {
       size_t filesize = phdr.p_filesz;
       size_t memsize = phdr.p_memsz;
       uint32_t paddr = phdr.p_paddr;
 
-      // Log("Loading segment: p_type = 0x%x, p_offset = 0x%x, p_vaddr = 0x%x, p_paddr = 0x%x, p_filesz = 0x%x, p_memsz = 0x%x, p_flags = 0x%x", 
-          // phdr.p_type, phdr.p_offset, phdr.p_vaddr, phdr.p_paddr, phdr.p_filesz, phdr.p_memsz, phdr.p_flags);
-      
+      // Log("Loading segment: p_type = 0x%x, p_offset = 0x%x, p_vaddr = 0x%x,
+      // p_paddr = 0x%x, p_filesz = 0x%x, p_memsz = 0x%x, p_flags = 0x%x",
+      // phdr.p_type, phdr.p_offset, phdr.p_vaddr, phdr.p_paddr, phdr.p_filesz,
+      // phdr.p_memsz, phdr.p_flags);
+
       size = fs_lseek(fd, phdr.p_offset, SEEK_SET);
-      size = fs_read(fd, (char *)paddr, filesize);
+      size = fs_read(fd, (char *)(uintptr_t)paddr, filesize);
       assert(size == filesize);
       assert(memsize >= filesize);
-      memset((char *)(paddr + filesize), 0, memsize - filesize);
+      memset((char *)(uintptr_t)(paddr + filesize), 0, memsize - filesize);
     }
   }
 
@@ -69,6 +74,5 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = %d", entry);
-  ((void(*)())entry) ();
+  ((void (*)())entry)();
 }
-
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -23,7 +23,7 @@ void init_proc() {
 
   Log("Initializing processes...");
 
-  naive_uload(NULL, "/bin/bird");
+  naive_uload(NULL, "/bin/hello");
 }
 
 Context *schedule(Context *prev) { return NULL; }
