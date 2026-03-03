d917a4f4229d56509c0fa3d5fea9f16eaa47d3be
tracer-ysyx <tracer@ysyx.org>
2026-03-03 19:46:00 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:46:00 up  4:33,  1 user,  load average: 0.33, 0.18, 0.12

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -7,6 +7,31 @@
 
 static const char *keyname[] = {"NONE", _KEYS(keyname)};
 
+static int sdl_strcmp(const char *str1, const char *str2) {
+  int i = 0;
+  while (str1[i] != '\0' && str2[i] != '\0') {
+    if (str1[i] != str2[i])
+      return 0;
+    i++;
+  }
+
+  return str1[i] == str2[i];
+}
+
+static int sdl_stoi(const char *str) {
+  int result = 0;
+
+  assert(*str >= '0' && *str <= '9');
+
+  while (*str >= '0' && *str <= '9') {
+    result = result * 10 + (*str - '0');
+    str++;
+  }
+
+  assert(*str == '\0');
+  return result;
+}
+
 int SDL_PushEvent(SDL_Event *ev) { return 0; }
 
 int SDL_PollEvent(SDL_Event *ev) { 
@@ -15,7 +40,7 @@ int SDL_PollEvent(SDL_Event *ev) {
 
   if(res == 0) return 0;
 
-  char *keystate = buf;
+  char *is_press = buf;
   char *keyn = buf + 3;
   buf[2] = '\0';
 
@@ -26,9 +51,9 @@ int SDL_PollEvent(SDL_Event *ev) {
     }
   }
 
-  if (sdl_strcmp(keystate, "kd")) {
+  if (sdl_strcmp(is_press, "kd")) {
     ev->key.type = SDL_KEYDOWN;
-  } else if (sdl_strcmp(keystate, "ku")) {
+  } else if (sdl_strcmp(is_press, "ku")) {
     ev->key.type = SDL_KEYUP;
   } else {
     assert(0);
@@ -42,17 +67,6 @@ int SDL_PollEvent(SDL_Event *ev) {
   }
 
   return 1; 
-}
-
-int sdl_strcmp(const char *str1, const char *str2) {
-  int i = 0;
-  while (str1[i] != '\0' && str2[i] != '\0') {
-    if (str1[i] != str2[i])
-      return 0;
-    i++;
-  }
-
-  return str1[i] == str2[i];
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
@@ -94,4 +108,42 @@ int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
   return 0;
 }
 
-uint8_t *SDL_GetKeyState(int *numkeys) { return NULL; }
+#define SDL_NUMKEYS (sizeof(keyname) / sizeof(keyname[0]))
+static uint8_t keystate[SDL_NUMKEYS];
+
+uint8_t *SDL_GetKeyState(int *numkeys) { 
+  char buf[16];
+  int res;
+
+  for(int i = 0; i < sizeof(keyname) / sizeof(keyname[0]); i++) {
+    keystate[i] = 0;
+  }
+
+  while((res = NDL_PollEvent(buf, 16)) != 0) {
+      char *is_press = buf;
+    char *keyn = buf + 3;
+    buf[2] = '\0';
+
+    for(int i = 3; i < 16; i++) {
+      if(buf[i] == '\n') {
+        buf[i] = '\0';
+        break;
+      }
+    }
+
+    int key_val = sdl_strcmp(is_press, "kd") ? 1 : 0;
+
+    if (sdl_strcmp(is_press, "kd")) {
+      for(int i = 0; i < sizeof(keyname) / sizeof(keyname[0]); i++) {
+        if(sdl_strcmp(keyname[i], keyn)) {
+          keystate[i] = key_val;
+          break;
+        }
+      }
+    } 
+  }
+
+  if(numkeys)
+    *numkeys = SDL_NUMKEYS;
+  return keystate;
+}
