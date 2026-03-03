831f2b3c307cef09c5ff865b34b9bb7e0ca0f363
tracer-ysyx <tracer@ysyx.org>
2026-03-03 16:04:46 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:04:46 up 52 min,  1 user,  load average: 0.32, 0.21, 0.19

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -317,6 +317,7 @@ inline uint32_t color_distance(uint8_t r0, uint8_t r1, uint8_t g0, uint8_t g1,
 int closest_color_idx(SDL_Palette *pa, uint8_t r, uint8_t g, uint8_t b) {
   uint32_t min_distance = -1;
   int min_idx = -1;
+  printf("[Debug] the address of pal is %p\n", pa);
   for (int i = 0; i < pa->ncolors; i++) {
     SDL_Color *color = pa->colors + i;
     uint32_t diff = color_distance(color->r, r, color->g, g, color->b, b);
@@ -336,7 +337,6 @@ uint32_t SDL_MapRGBA(SDL_PixelFormat *fmt, uint8_t r, uint8_t g, uint8_t b,
   int bytes_per_pixel = fmt->BytesPerPixel;
   int bits_per_pixel = fmt->BitsPerPixel;
   assert(bits_per_pixel != 0 || bytes_per_pixel != 0);
-
   
   if (bytes_per_pixel == 1) {
     assert(fmt->palette);
