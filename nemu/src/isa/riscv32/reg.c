6b1ee684211bf864ae5be45cd03912e8576f5865
tracer-ysyx <tracer@ysyx.org>
2026-03-04 20:07:17 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:07:17 up  7:38,  1 user,  load average: 0.16, 0.23, 0.18

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -31,10 +31,11 @@ void isa_reg_display(CPU_state *ref) {
   if (ref == NULL) {
     // 只打印本机寄存器
     printf("%s  %-5s %-12s %-12s%s\n", C_HDR, "reg", "hex", "dec", C_RESET);
-    printf("  %s%-5s %-12s %-12s%s\n", C_HDR, "-----", "------------", "------------", C_RESET);
+    printf("  %s%-5s %-12s %-12s%s\n",
+           C_HDR, "-----", "------------", "------------", C_RESET);
     for (int i = 0; i < 32; i++) {
       uint32_t val = cpu.gpr[i];
-      printf("  %s%-5s%s %s0x%08x%s %s%-12d%s\n",
+      printf("  %s%-5s%s %s0x%08x%s   %s%-12d%s\n",
              C_NAME, regs[i], C_RESET,
              C_VAL,  val,      C_RESET,
              C_VAL,  (int32_t)val, C_RESET);
@@ -52,14 +53,14 @@ void isa_reg_display(CPU_state *ref) {
       uint32_t d = cpu.gpr[i];
       uint32_t r = ref->gpr[i];
       const char *c = (d == r) ? C_VAL : C_DIFF;
-      printf("  %s%-5s%s %s0x%08x%s %s0x%08x%s %s%-8d%s\n",
+      printf("  %s%-5s%s %s0x%08x%s   %s0x%08x%s   %s%-8d%s\n",
              C_NAME, regs[i], C_RESET,
              c,      d,       C_RESET,
              c,      r,       C_RESET,
              c,      (int32_t)d - (int32_t)r, C_RESET);
     }
     const char *cpc = (cpu.pc == ref->pc) ? C_VAL : C_DIFF;
-    printf("  %s%-5s%s %s0x%08x%s %s0x%08x%s\n",
+    printf("  %s%-5s%s %s0x%08x%s   %s0x%08x%s\n",
            C_NAME, "pc", C_RESET,
            cpc,     cpu.pc, C_RESET,
            cpc,     ref->pc, C_RESET);
