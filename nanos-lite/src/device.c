d5b1c5eea0317bdc7b2b610663e194270a67ed58
tracer-ysyx <tracer@ysyx.org>
2026-03-12 22:20:29 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:20:29 up  7:43,  1 user,  load average: 0.18, 0.20, 0.23

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,6 +15,7 @@ static const char *keyname[256]
     __attribute__((used)) = {[AM_KEY_NONE] = "NONE", AM_KEYS(NAME)};
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
+  yield();
   int write_counter = 0;
   for (write_counter = 0; write_counter < len; write_counter++) {
     putch(((char *)buf)[write_counter]);
@@ -39,6 +40,7 @@ int get_time(struct timeval *tv, struct timezone *tz) {
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
+  yield();
   AM_INPUT_KEYBRD_T kbd;
   ioe_read(AM_INPUT_KEYBRD, &kbd);
 
@@ -63,6 +65,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
+  yield();
   AM_GPU_CONFIG_T cfg;
   ioe_read(AM_GPU_CONFIG, &cfg);
 
