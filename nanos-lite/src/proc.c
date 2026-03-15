991264b7b38aa795e67045e2885acbb7214d5059
tracer-ysyx <tracer@ysyx.org>
2026-03-16 00:38:26 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:38:26 up  6:11,  1 user,  load average: 0.23, 0.24, 0.32

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -16,7 +16,7 @@ void hello_fun(void *arg) {
   while (1) {
     Log("Hello World from Nanos-lite with arg '%s' for the %dth time!",
         (char *)arg, j);
-    j++;
+    assert(j == 1);
     yield();
   }
 }
