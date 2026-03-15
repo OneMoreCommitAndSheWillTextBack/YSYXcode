a3566286e7c11a651cfb5c7d04d2e3f216ab31e1
tracer-ysyx <tracer@ysyx.org>
2026-03-16 00:41:51 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:41:51 up  6:15,  1 user,  load average: 0.73, 0.50, 0.41

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -14,7 +14,7 @@ void switch_boot_pcb() { current = &pcb_boot; }
 void hello_fun(void *arg) {
   int j = 1;
   while (1) {
-    Log("Hello World from Nanos-lite with arg '%s' for the %dth time!",
+    Log("Hello World from Nanos-lite with arg '%s' for the %pth time!",
         (char *)arg, j);
     j++;
     if(j == 10) {
