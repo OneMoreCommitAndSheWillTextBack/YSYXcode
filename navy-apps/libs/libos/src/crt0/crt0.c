af11b0abb3bdf7b19f5ea5d06f3dab91b32d1901
sunmingyang <2326103265@qq.com>
2026-03-19 22:09:16 +0800

    fix the argdeal_uload bug

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -2,11 +2,31 @@
 #include <stdlib.h>
 #include <assert.h>
 
+#define MAXARG 32
+#define MAXENVP 32
+
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-  char *empty[] =  {NULL };
-  environ = empty;
-  exit(main(0, empty, empty));
+  char *argv[MAXARG];
+  char *envp[MAXENVP];
+  int argc = *(uint32_t *)args;
+
+  char **ptr_array = (char **)(args + 1);
+  int argv_count = 0;
+  while (*ptr_array != NULL && argv_count < MAXARG) {
+    argv[argv_count++] = *ptr_array++;
+  }
+  argv[argv_count] = NULL;  
+  ptr_array++;              
+
+  int envp_count = 0;
+  while (*ptr_array != NULL && envp_count < MAXENVP) {
+    envp[envp_count++] = *ptr_array++;
+  }
+  envp[envp_count] = NULL;  
+
+  environ = envp;
+  exit(main(argc, argv, envp));
   assert(0);
 }
