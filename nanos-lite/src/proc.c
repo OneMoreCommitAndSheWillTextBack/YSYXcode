2f2fb83f3880a8a4f1b11e83a94c537ea2f01f65
tracer-ysyx <tracer@ysyx.org>
2026-03-20 14:26:44 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux ysyx 6.5.0-45-generic #45~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 15 16:40:02 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:26:44 up 49 min,  1 user,  load average: 0.65, 0.53, 0.36

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -121,9 +121,9 @@ void init_proc() {
 
   // naive_uload(NULL, "/bin/nterm");
   context_kload(&pcb[0], hello_fun, "A");
-  char *argv[] = {"--skip", NULL};
+  // char *argv[] = {"--skip", NULL};
   char *envp[] = {"PATH=/bin", NULL};
-  context_uload(&pcb[1], "/bin/pal", argv, envp);
+  context_uload(&pcb[1], "/bin/nterm", NULL, envp);
 }
 
 Context *schedule(Context *prev) {
