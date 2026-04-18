499a10c333039352a76503012643679d630b55e8
tracer-ysyx <tracer@ysyx.org>
2026-04-18 23:09:18 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux cinder-Xiaomi-Book-Pro-14 7.0.0-14-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Mon Apr 13 11:09:53 UTC 2026 x86_64 GNU/Linux  23:09:18 up  5:40,  1 user,  load average: 1.49, 1.40, 1.42

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,15 +9,14 @@ void __am_switch(Context *c);
 
 static void dump_context(const char *tag, Context *c) {
   printf("[cte] %s: c=%p pdir=%p mcause=%08x mstatus=%08x mepc=%08x\n", tag, c,
-         c->pdir, (uint32_t)c->mcause, (uint32_t)c->mstatus,
-         (uint32_t)c->mepc);
+         c->pdir, (uint32_t)c->mcause, (uint32_t)c->mstatus, (uint32_t)c->mepc);
   for (int i = 0; i < NR_REGS; i++) {
     printf("[cte] %s: gpr[%02d]=%08x\n", tag, i, (uint32_t)c->gpr[i]);
   }
 }
 
 Context *__am_irq_handle(Context *c) {
-  // printf("into am irq handle, mcause is %d\n", c->mcause);
+  printf("into am irq handle, mcause is %d\n", c->mcause);
   __am_get_cur_as(c);
   dump_context("trap-enter", c);
   if (user_handler) {
