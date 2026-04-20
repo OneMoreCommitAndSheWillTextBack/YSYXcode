c6fada45347a1172e176087f6424977aab61d57f
tracer-ysyx <tracer@ysyx.org>
2026-04-20 22:42:37 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux cinder-Xiaomi-Book-Pro-14 7.0.0-14-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Mon Apr 13 11:09:53 UTC 2026 x86_64 GNU/Linux  22:42:37 up  9:26,  1 user,  load average: 2.41, 2.51, 2.28

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -18,8 +18,8 @@ uint32_t NDL_GetTicks() {
   struct timeval tv;
   struct timezone tz;
 
-  int res = gettimeofday(&tv, &tz);
-  return tv.tv_usec / 1000;
+  gettimeofday(&tv, &tz);
+  return (uint32_t)(tv.tv_sec * 1000 + tv.tv_usec / 1000);
 }
 
 int NDL_PollEvent(char *buf, int len) {
@@ -34,48 +34,56 @@ static size_t canvas_width = 0;
 static size_t canvas_height = 0;
 static size_t canvas_x = 0;
 static size_t canvas_y = 0;
-void NDL_OpenCanvas(int *w, int *h) {
-  int fd = open("/proc/dispinfo", 0, 0);
-  char buf[128];
-  int readn = read(fd, buf, sizeof(buf));
-  close(fd);
-  if(readn == 0) {
-    *w = 0; *h = 0; return ;
-  }
-  int max_height, max_width;
 
-  sscanf(buf, "WIDTH : %d\nHEIGHT:%d", &max_width, &max_height);
-  if(*w == 0) *w = max_width;
-  if(*h == 0) *h = max_height;
+static void read_dispinfo(int *w, int *h) {
+  int fd = open("/proc/dispinfo", 0, 0);
+  assert(fd >= 0);
+
+  char buf[128];
+  int readn = read(fd, buf, sizeof(buf) - 1);
+  close(fd);
+  assert(readn > 0);
+  buf[readn] = '\0';
+
+  // Be tolerant to spaces around ':' because different backends may format
+  // dispinfo slightly differently.
+  int ok = sscanf(buf, "WIDTH : %d\nHEIGHT : %d", w, h);
+  if (ok != 2) {
+    ok = sscanf(buf, "WIDTH:%d\nHEIGHT:%d", w, h);
+  }
+  assert(ok == 2);
+}
+
+void NDL_OpenCanvas(int *w, int *h) {
+  int max_height = 0, max_width = 0;
+  read_dispinfo(&max_width, &max_height);
+  if (*w == 0) *w = max_width;
+  if (*h == 0) *h = max_height;
 
   assert(*w <= max_width);
   assert(*h <= max_height);
 
+  screen_w = max_width;
+  screen_h = max_height;
   canvas_width = *w;
   canvas_height = *h;
-  return ;
+  return;
 }
 
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
-  int fd_info = open("/proc/dispinfo", 0, 0);
-  char buf[128];
-  int readn = read(fd_info, buf, sizeof(buf));
-  while(readn == 0) {
-    readn = read(fd_info, buf, sizeof(buf));
+  if (screen_w == 0 || screen_h == 0) {
+    read_dispinfo(&screen_w, &screen_h);
   }
-  close(fd_info);
-  int max_height, max_width;
-  sscanf(buf, "WIDTH : %d\nHEIGHT:%d", &max_width, &max_height);
 
   int frame_buffer_x = canvas_x + x;
   int frame_buffer_y = canvas_y + y;
 
-  if(h == 0) h = max_height;
-  if(w == 0) w = max_width;
+  if (h == 0) h = canvas_height;
+  if (w == 0) w = canvas_width;
 
   int fd_fb = open("/dev/fb", 0, 0);
-  for(int i = 0; i < h; i++) {
-    lseek(fd_fb, sizeof(uint32_t) * ((frame_buffer_y + i) * max_width + frame_buffer_x), SEEK_SET);
+  for (int i = 0; i < h; i++) {
+    lseek(fd_fb, sizeof(uint32_t) * ((frame_buffer_y + i) * screen_w + frame_buffer_x), SEEK_SET);
     write(fd_fb, (pixels + i * w), sizeof(uint32_t) * w);
   }
   close(fd_fb);
