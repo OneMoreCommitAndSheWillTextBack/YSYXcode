ec6060e3b44174520f6e46d55eeb2dc61d3d22d9
tracer-ysyx <tracer@ysyx.org>
2026-04-20 16:52:49 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux cinder-Xiaomi-Book-Pro-14 7.0.0-14-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Mon Apr 13 11:09:53 UTC 2026 x86_64 GNU/Linux  16:52:49 up  3:36,  1 user,  load average: 0.62, 0.45, 0.45

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -13,6 +13,7 @@ static Context *do_event(Event e, Context *c) {
     break;
   case EVENT_IRQ_TIMER:
     c = schedule(c);
+    break;
   default:
     panic("Unhandled event ID = %d", e.event);
   }
