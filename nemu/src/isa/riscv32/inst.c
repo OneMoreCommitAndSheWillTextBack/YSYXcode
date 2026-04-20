b63d56540e8c1a6de49f8cf54ceafabd884e410d
tracer-ysyx <tracer@ysyx.org>
2026-04-20 13:46:40 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux cinder-Xiaomi-Book-Pro-14 7.0.0-14-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Mon Apr 13 11:09:53 UTC 2026 x86_64 GNU/Linux  13:46:40 up 30 min,  1 user,  load average: 0.33, 0.33, 0.28

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -217,11 +217,14 @@ static word_t mret_inst() {
   return cpu.csr.mepc; 
 }
 
+#define CSR_MAST 0xfff
 static uint32_t csr_read(uint32_t csr_num) {
+  csr_num &= CSR_MAST;
   return *(get_csr(csr_num));
 }
 
 static void csr_write(uint32_t csr_num, uint32_t data) {
+  csr_num &= CSR_MAST;
   *(get_csr(csr_num)) = data;
   return;
 }
