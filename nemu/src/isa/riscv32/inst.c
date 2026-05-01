7a076947369c5433f899deb5542a441caa67e41a
tracer-ysyx <tracer@ysyx.org>
2026-05-01 15:58:27 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux cinder-Xiaomi-Book-Pro-14 7.0.0-14-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Mon Apr 13 11:09:53 UTC 2026 x86_64 GNU/Linux  15:58:27 up  4:22,  1 user,  load average: 1.52, 1.16, 1.41

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -143,6 +143,8 @@ static int decode_exec(Decode *s) {
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("0000000 00000 00000 001 00000 00011 11", fence.i, I, );
+  INSTPAT("0000??? ????? 00000 000 00000 00011 11", fence  , I, );
+  
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
   INSTPAT_END();
 
