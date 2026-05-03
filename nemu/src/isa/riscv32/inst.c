438d7ce96353b5a1f52879d5b212cfedd98fabbe
tracer-ysyx <tracer@ysyx.org>
2026-05-03 12:02:54 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux cinder-Xiaomi-Book-Pro-14 7.0.0-14-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Mon Apr 13 11:09:53 UTC 2026 x86_64 GNU/Linux  12:02:54 up  1:19,  1 user,  load average: 0.77, 0.68, 0.63

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -229,7 +229,6 @@ static int decode_exec_c(Decode *s) {
 int isa_exec_once(Decode *s) {
   s->isa.inst.val = inst_fetch(&s->snpc, 2);
   if((s->isa.inst.val & 0b11 ) != 3) {
-    s->snpc = cpu.pc + 2;
     return decode_exec_c(s);
   }
 
