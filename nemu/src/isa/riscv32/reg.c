91e694ff2c1df8ebdb98bc39448da732cf8aec9f
tracer-ysyx <tracer@ysyx.org>
2026-05-05 16:56:03 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux cinder-Xiaomi-Book-Pro-14 7.0.0-14-generic #14-Ubuntu SMP PREEMPT_DYNAMIC Mon Apr 13 11:09:53 UTC 2026 x86_64 GNU/Linux  16:56:03 up  6:06,  1 user,  load average: 0.72, 0.65, 0.40

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -22,8 +22,8 @@ const char *regs[] = {"$0", "ra", "sp",  "gp",  "tp", "t0", "t1", "t2",
                       "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
 
 #define DIFF_CSR_LIST                                                          \
-  X(mstatus) X(mcause) X(mepc) X(mtvec) X(satp) X(mscratch) X(mie)
-#define DIFF_CSR_NUM 7
+  X(mstatus) X(mcause) X(mepc) X(mtvec) X(satp) X(mscratch) X(mie) X(scause)
+#define DIFF_CSR_NUM 8
 
 const char *csr[] = {
 #define X(name) #name,
