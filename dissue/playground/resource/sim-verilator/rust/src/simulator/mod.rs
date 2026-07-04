8d8a52fa107c69723d82fa8be7a1ec3e537940c9
tracer-ysyx <tracer@ysyx.org>
2026-07-04 20:37:05 +0800

    > generate verilog ysyx_24100007 孙明扬 Linux cinder-Xiaomi-Book-Pro-14 7.0.0-27-generic #27-Ubuntu SMP PREEMPT_DYNAMIC Thu Jun 18 19:13:49 UTC 2026 x86_64 GNU/Linux  20:37:05 up 12:13,  1 user,  load average: 1.58, 0.99, 1.10

--- dissue/playground/resource/sim-verilator/rust/src/simulator/mod.rs
+++ dissue/playground/resource/sim-verilator/rust/src/simulator/mod.rs
@@ -246,7 +246,7 @@ impl Simulator {
         for commit in event.valid_inst_pc() {
             let pc = commit.0;
             let inst = commit.1;
-            println!("commit inst 0x{inst:08x} at pc 0x{pc:08x}");
+            // println!("commit inst 0x{inst:08x} at pc 0x{pc:08x}");
         }
 
         if let Err(error) = self.do_difftest(commit_count) {
