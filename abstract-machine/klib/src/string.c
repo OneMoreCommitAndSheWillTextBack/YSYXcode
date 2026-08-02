14b491dba6601f0002e82d27ad4a64db314f0d55
tracer-ysyx <tracer@ysyx.org>
2026-08-02 19:33:08 +0800

    > compile NEMU ysyx_24100007 孙明扬 Linux cinder-Xiaomi-Book-Pro-14 7.0.0-28-generic #28-Ubuntu SMP PREEMPT_DYNAMIC Sun Jun 21 01:01:36 UTC 2026 x86_64 GNU/Linux  19:33:08 up 4 days,  5:04,  1 user,  load average: 1.38, 1.36, 1.07

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -68,6 +68,17 @@ int strncmp(const char *s1, const char *s2, size_t n) {
   return 0;
 }
 
+char  *strchr    (const char *s, int c) {
+  char *ptr = (char *)s;
+  while(*ptr != '\0') {
+    if(*ptr == c) {
+      return ptr;
+    }
+  }
+
+  return NULL;
+}
+
 void *memset(void *s, int c, size_t n) { 
   char *pos = (char*)s;
   for(size_t i=0;i<n;i++){
--- rt-thread-am/bsp/abstract-machine/src/libc.c
+++ rt-thread-am/bsp/abstract-machine/src/libc.c
@@ -1,8 +1,6 @@
 #include <klib.h>
 #include <rtthread.h>
 
-char *strchr(const char *s, int c) { assert(0); }
-
 char *strrchr(const char *s, int c) { assert(0); }
 
 char *strstr(const char *haystack, const char *needle) {
